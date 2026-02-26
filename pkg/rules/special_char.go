b8568f9385c59a22c02281823084b45e3f567fa7
mmmIlia <tiunov.i.v@gmail.com>
2026-02-26 17:37:28 +0300

    feat: add suggested fix for special characters rule

--- pkg/rules/special_char.go
+++ pkg/rules/special_char.go
@@ -27,20 +27,52 @@ func (r *SpecialCharsRule) Check(pass *analysis.Pass, msgArg ast.Expr) {
 		return
 	}
 
+	var firstErrorMsg string
+	
 	if strings.HasSuffix(val, ".") {
-		pass.Reportf(lit.Pos(), "log message should not end with punctuation")
-		return
+		firstErrorMsg = "log message should not end with punctuation"
 	}
 
-	for _, r := range val {
-		if unicode.In(r, unicode.So, unicode.Sk) {
-			pass.Reportf(lit.Pos(), "log message should not contain emojis or special symbols")
-			return
-		}
+	cleanVal := strings.TrimSuffix(val, ".")
+	var sb strings.Builder
+	
+	for _, runeValue := range cleanVal {
+		isEmoji := unicode.In(runeValue, unicode.So, unicode.Sk)
+		isNoisy := strings.ContainsRune("!?", runeValue)
 
-		if strings.ContainsRune("!?", r) {
-			pass.Reportf(lit.Pos(), "log message should not contain exclamation or question marks")
-			return
+		if isEmoji || isNoisy {
+			if firstErrorMsg == "" {
+				if isEmoji {
+					firstErrorMsg = "log message should not contain emojis or special symbols"
+				} else {
+					firstErrorMsg = "log message should not contain exclamation or question marks"
+				}
+			}
+			continue
 		}
+		sb.WriteRune(runeValue)
+	}
+
+	if firstErrorMsg != "" {
+		finalStr := sb.String()
+		newLit := strconv.Quote(finalStr)
+
+		pass.Report(analysis.Diagnostic{
+			Pos:     lit.Pos(),
+			End:     lit.End(),
+			Message: firstErrorMsg,
+			SuggestedFixes: []analysis.SuggestedFix{
+				{
+					Message: "Clean up log message",
+					TextEdits: []analysis.TextEdit{
+						{
+							Pos:     lit.Pos(),
+							End:     lit.End(),
+							NewText: []byte(newLit),
+						},
+					},
+				},
+			},
+		})
 	}
 }
