70713aa2fee0f9c47096e341bfe64ba4fd9499a1
mmmIlia <tiunov.i.v@gmail.com>
2026-02-28 19:37:25 +0300

    test: check error return values in analyzer tests

--- pkg/analyzer/analyzer_test.go
+++ pkg/analyzer/analyzer_test.go
@@ -3,13 +3,16 @@ package analyzer_test
 import (
 	"testing"
 
-	"github.com/mmmIlia/loglinter/pkg/analyzer"
 	"golang.org/x/tools/go/analysis/analysistest"
+
+	"github.com/mmmIlia/loglinter/pkg/analyzer"
 )
 
 func TestAnalyzer(t *testing.T) {
 	testdata := analysistest.TestData()
-	analysistest.RunWithSuggestedFixes(t, testdata, analyzer.NewAnalyzer(), "a")
+
+	a := analyzer.NewAnalyzer()
+	analysistest.RunWithSuggestedFixes(t, testdata, a, "a")
 }
 
 func TestAnalyzer_ConfigDisabled(t *testing.T) {
@@ -17,9 +20,15 @@ func TestAnalyzer_ConfigDisabled(t *testing.T) {
 
 	a := analyzer.NewAnalyzer()
 
-	a.Flags.Set("disable-lowercase", "true")
-	a.Flags.Set("disable-english", "true")
-	a.Flags.Set("disable-special-chars", "true")
+	if err := a.Flags.Set("disable-lowercase", "true"); err != nil {
+		t.Fatal(err)
+	}
+	if err := a.Flags.Set("disable-english", "true"); err != nil {
+		t.Fatal(err)
+	}
+	if err := a.Flags.Set("disable-special-chars", "true"); err != nil {
+		t.Fatal(err)
+	}
 
 	analysistest.Run(t, testdata, a, "config_test")
 }
@@ -28,7 +37,10 @@ func TestAnalyzer_CustomPatterns(t *testing.T) {
 	testdata := analysistest.TestData()
 
 	a := analyzer.NewAnalyzer()
-	a.Flags.Set("sensitive-patterns", "email,ssn")
+
+	if err := a.Flags.Set("sensitive-patterns", "email,ssn"); err != nil {
+		t.Fatal(err)
+	}
 
 	analysistest.Run(t, testdata, a, "custom_patterns")
 }
