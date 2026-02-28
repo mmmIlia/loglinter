118866045a894cfb3a6692e7af6b71bbd55cd6a4
mmmIlia <tiunov.i.v@gmail.com>
2026-02-28 18:50:50 +0300

    feat: add plugin

---
+++ plugin/plugin.go
@@ -0,0 +1,34 @@
+package plugin
+
+import (
+	"golang.org/x/tools/go/analysis"
+
+	"github.com/mmmIlia/loglinter/pkg/analyzer"
+)
+
+const Name = "loglinter"
+
+func New(conf any) ([]*analysis.Analyzer, error) {
+	a := analyzer.NewAnalyzer()
+
+	if settings, ok := conf.(map[string]any); ok {
+		if val, ok := settings["disable-lowercase"].(bool); ok && val {
+			a.Flags.Set("disable-lowercase", "true")
+		}
+		if val, ok := settings["disable-english"].(bool); ok && val {
+			a.Flags.Set("disable-english", "true")
+		}
+		if val, ok := settings["disable-special-chars"].(bool); ok && val {
+			a.Flags.Set("disable-special-chars", "true")
+		}
+		if val, ok := settings["disable-sensitive"].(bool); ok && val {
+			a.Flags.Set("disable-sensitive", "true")
+		}
+		
+		if val, ok := settings["sensitive-patterns"].(string); ok {
+			a.Flags.Set("sensitive-patterns", val)
+		}
+	}
+
+	return []*analysis.Analyzer{a}, nil
+}
