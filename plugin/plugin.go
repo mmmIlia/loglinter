0857e012fbca2106dde4e12174356c7cc1d6cb13
mmmIlia <tiunov.i.v@gmail.com>
2026-02-28 19:35:12 +0300

    refactor: check error return values in plugin initialization

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,32 +1,30 @@
 package plugin
 
 import (
+	"fmt"
+
 	"golang.org/x/tools/go/analysis"
 
 	"github.com/mmmIlia/loglinter/pkg/analyzer"
 )
 
-const Name = "loglinter"
-
 func New(conf any) ([]*analysis.Analyzer, error) {
 	a := analyzer.NewAnalyzer()
 
-	if settings, ok := conf.(map[string]any); ok {
-		if val, ok := settings["disable-lowercase"].(bool); ok && val {
-			a.Flags.Set("disable-lowercase", "true")
-		}
-		if val, ok := settings["disable-english"].(bool); ok && val {
-			a.Flags.Set("disable-english", "true")
-		}
-		if val, ok := settings["disable-special-chars"].(bool); ok && val {
-			a.Flags.Set("disable-special-chars", "true")
-		}
-		if val, ok := settings["disable-sensitive"].(bool); ok && val {
-			a.Flags.Set("disable-sensitive", "true")
-		}
+	if conf == nil {
+		return []*analysis.Analyzer{a}, nil
+	}
 
-		if val, ok := settings["sensitive-patterns"].(string); ok {
-			a.Flags.Set("sensitive-patterns", val)
+	settings, ok := conf.(map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("loglinter: configuration must be a map, got %T", conf)
+	}
+
+	for key, value := range settings {
+		strValue := fmt.Sprintf("%v", value)
+
+		if err := a.Flags.Set(key, strValue); err != nil {
+			return nil, fmt.Errorf("loglinter: invalid setting %q: %w", key, err)
 		}
 	}
 
